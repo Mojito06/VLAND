416b7abd40edb78adc2a1c8409b922f1279bdc74
Mojito06 <louis.papot@outlook.fr>
2023-01-16 19:36:47 +0100

    normalement ca marche mais mon pc est a la ramasse et il est en train de cramer depuis 1h, donc jeprefere commit mdrr

--- app/src/main/java/com/example/vigiflash/MainActivity.java
+++ app/src/main/java/com/example/vigiflash/MainActivity.java
@@ -1,17 +1,34 @@
 package com.example.vigiflash;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import java.io.*;
+import java.net.*;
+import org.json.JSONObject;
+import org.json.JSONArray;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.green_activity);
+        setContentView(R.layout.activity_main);
+        System.out.println("coucouc");
+        try {
+            TestAsyncTask testAsyncTask = new TestAsyncTask(MainActivity.this, "http://isis.unice.fr/~pl105229/ext/sae302/db.php");
+            testAsyncTask.execute();
+
+        }
+        catch(Exception ex) {
+            ex.printStackTrace();
+            System.out.println("bruh");
+
+        }
     }
 
     public void buttonJour1 (View v) {
@@ -39,5 +56,81 @@ public class MainActivity extends AppCompatActivity {
     private void switchRedActivities() {
         Intent switchActivityIntent = new Intent(this, RedActivity.class);
         startActivity(switchActivityIntent);
+    }
+
+    public class TestAsyncTask extends AsyncTask<Void, Void, String> {
+        private Context mContext;
+        private String mUrl;
+
+        public TestAsyncTask(Context context, String url) {
+            mContext = context;
+            mUrl = url;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+            String resultString = "Done";
+            getJSON(mUrl);
+
+            return resultString;
+        }
+
+        @Override
+        protected void onPostExecute(String strings) {
+            super.onPostExecute(strings);
+        }
+
+        private void getJSON(String url) {
+            try {
+                //Connexion au site
+                URL the_url = new URL(url);
+                HttpURLConnection conn = (HttpURLConnection) the_url.openConnection();
+                conn.setRequestMethod("GET");
+                System.out.println("get");
+                conn.setRequestProperty("Accept", "application/json");
+                System.out.println("part1");
+
+                if (conn.getResponseCode() != 200) {
+                    throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode());
+                }
+                // Création du buffer pour lire le contenue du site
+                BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+                System.out.println("buffer");
+                StringBuilder sb = new StringBuilder();
+                System.out.println("stringbuilder");
+                String output;
+                System.out.println("part3");
+                // On crée une liste "sb" ou on vient rajouter les infos du site.
+                while ((output = br.readLine()) != null) {
+                    sb.append(output);
+                }
+                conn.disconnect();
+                System.out.println("disconnect");
+                // On crée un variable jsonText qui va nous permettre d'exploiter le JSON.
+                String jsonText = sb.toString();
+
+                JSONObject json = new JSONObject(jsonText);
+
+                // On récupére Le jour et le day value de la chaine de charactère jsonText
+                JSONArray signals = json.getJSONArray("signals");
+                for (int i = 0; i < signals.length(); i++) {
+                    JSONObject signal = signals.getJSONObject(i);
+                    String jour = signal.getString("jour");
+                    int dvalue = signal.getInt("dvalue");
+                    System.out.println("Jour: " + jour + ", alerte niveau : " + dvalue);
+                }
+            }
+            catch(Exception ex) {
+                ex.printStackTrace();
+                System.out.println("bruh");
+
+            }
+
+        }
     }
 }
